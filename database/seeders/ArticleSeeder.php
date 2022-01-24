5f1d6352cae66648bc184e0526e448052617e17b
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-01-24 16:39:17 +0100

    seeder ArticleSeeder + migration

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+
+        $faker = Factory::create();
+
+        for ($i=0; $i < 25 ; $i++) { 
+            Article::create([
+                'title' => $faker->sentence(),
+                'subtitle' => $faker->sentence(),
+                'content' => $faker->text($maxNbChars = 600),
+            ]);
+        }
+
+        
+    }
+}
