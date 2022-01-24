5927f4cb6d43b99b1f8732bb0946da6ceadc5b93
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-01-24 15:36:18 +0100

    project initialization - front-end part - controller & migration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-2">
+        <div class="alert alert-dismissible alert-warning">
+                <h1 class="display-4">Livewire Blog</h1>
+                <p class="text-primary">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+                <p class="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="#" class="alert-link">vel scelerisque nisl consectetur</a>.</p>
+            <div>         
+                <img class="m-1 img-fluid rounded" src="https://laravelarticle.com/filemanager/uploads/laravel-livewire.png" alt="" style="width:30%">
+                <h5 class="card-title">Litlle TITLE of the card</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a type="link" class="btn btn-warning">Learn more <i class="fas fa-arrow-right"></i></a>
+            </div>
+        </div>  
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="" />
+	<meta name="description" content="" />    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Livewire-Blog') }}</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/livewire.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/livewire.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/livewire.png">
+    <link rel="mask-icon" href="/favicon/livewire.png" color="#ff2d20">
+    <link rel="shortcut icon" href="favicon/favicon.png">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- FONTAWESOME JS -->
+    <script src="{{ asset('lib/fontawesome.js') }}" defer></script>
+    @yield('head')
+</head>
+<body>
+    @include('components.navbar')
+
+    <div id="app">        
+
+        @yield('content')
+
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+</body>
+</html>
